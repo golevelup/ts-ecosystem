47f4ebce2d52241bbf1cf45a3b401a586e78e1ba
Jesse Carter <jesse.r.carter@gmail.com>
2020-05-09 15:53:00 -0400

    test

--- libs/ts-config/src/lib/configFactory.ts
+++ libs/ts-config/src/lib/configFactory.ts
@@ -1,4 +1,5 @@
 import * as path from 'path';
+import * as fs from 'fs';
 import * as dotenv from 'dotenv';
 import * as convict from 'convict';
 
@@ -11,13 +12,39 @@ export interface EnvConfig {
 
 export type NodeEnvConfigMap<T extends string> = Partial<Record<T, EnvConfig>>;
 
+export interface ConfigLoadOptions<T extends string = CommonNodeEnv> {
+  environmentConfigFiles?: NodeEnvConfigMap<T>;
+  strict?: boolean;
+  verbose?: boolean;
+}
+
+const defaultConfigLoadOptions: ConfigLoadOptions = {
+  strict: true,
+  verbose: false
+};
+
+const makeConditionalLogger = (enabled: boolean) => (message: string) => {
+  if (enabled) {
+    console.log(`@golevelup/ts-config: ${message}`);
+  }
+};
+
 export const createConfig = <K, T extends string = CommonNodeEnv>(
   schema: convict.Schema<K>,
-  environmentConfigFiles?: NodeEnvConfigMap<T>
+  configLoadOptions?: ConfigLoadOptions<T>
 ): convict.Config<K> => {
+  const { verbose, strict, environmentConfigFiles } = {
+    ...defaultConfigLoadOptions,
+    ...(configLoadOptions || {})
+  };
+  const log = makeConditionalLogger(verbose);
+
   const nodeEnv = (process.env.NODE_ENV || 'development') as T;
 
-  const envConfigMap = environmentConfigFiles[nodeEnv];
+  log(`Loading config in Node Env '${nodeEnv}'`);
+
+  const envConfigMap = (environmentConfigFiles || {})[nodeEnv];
+
   const parsedDotEnvPaths = envConfigMap?.dotEnvPaths || [];
   const parsedJsonPaths = envConfigMap?.jsonPaths || [];
 
@@ -29,16 +56,32 @@ export const createConfig = <K, T extends string = CommonNodeEnv>(
     ? parsedJsonPaths
     : [parsedJsonPaths];
 
-  dotEnvPaths.forEach(x => {
-    const dotEnvPath = path.isAbsolute(x) ? x : path.join(__dirname, x);
-    dotenv.config({ path: dotEnvPath });
-  });
-
   const config = convict<K>(schema);
 
   jsonPaths.forEach(x => {
     const jsonPath = path.isAbsolute(x) ? x : path.join(__dirname, x);
+    log(`Attempting to load JSON config file at: '${x}'`);
     config.loadFile(jsonPath);
+  });
+
+  dotEnvPaths.forEach(x => {
+    const dotEnvPath = path.isAbsolute(x) ? x : path.join(__dirname, x);
+    const exists = fs.existsSync(dotEnvPath);
+    if (!exists && strict) {
+      throw new Error(`Could not find file ${dotEnvPath}`);
+    }
+
+    log(
+      `Attempting to load .env file at: '${dotEnvPath}'. ${
+        exists ? `Found file` : `File not found. Skipping`
+      }`
+    );
+
+    if (exists) {
+      dotenv.config({ path: dotEnvPath });
+    }
+
+    console.log(process.env);
   });
 
   const validated = config.validate({
---
+++ libs/ts-config/src/test/configFactory.spec.ts
@@ -0,0 +1,59 @@
+import { createConfig } from '../lib/configFactory';
+import * as convict from 'convict';
+
+interface TestConfig {
+  name: string;
+  age: number;
+  nested: {
+    value: string;
+  };
+}
+
+const schema: convict.Schema<TestConfig> = {
+  name: {
+    default: 'Jesse',
+    env: 'NX_THIS_IS_NAME_ENV'
+  },
+  age: 30,
+  nested: {
+    value: 'awesome'
+  }
+};
+
+describe('ts-config', () => {
+  it('throws errors for missing .env files in strict mode', () => {
+    expect(() =>
+      createConfig<TestConfig>(schema, {
+        environmentConfigFiles: {
+          test: {
+            dotEnvPaths: ['asdfasdf']
+          }
+        },
+        verbose: true,
+        strict: true
+      })
+    ).toThrow();
+  });
+
+  it.only('loads and uses env files', () => {
+    const config = createConfig<TestConfig>(schema, {
+      verbose: true,
+      strict: true,
+      environmentConfigFiles: {
+        test: {
+          dotEnvPaths: ['../test/test.env']
+        }
+      }
+    });
+
+    expect(config.get('name')).toBe('WonderPanda');
+  });
+
+  it.skip('loads json files', () => {
+    expect(42).toBe(41);
+  });
+
+  it.skip('respects json -> .env -> env vars precedence', () => {
+    expect(42).toBe(41);
+  });
+});
